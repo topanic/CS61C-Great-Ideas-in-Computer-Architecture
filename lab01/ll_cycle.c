ec45048b5def900bbe1309d07b26447d14af2bb3
Arch <topanic@qq.com>
2024-02-16 18:18:34 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) return 0;
+    node *tortoise = head, *hare = head;
+    do {
+        tortoise = tortoise->next->next;
+        if (tortoise == NULL) {
+            return 0;
+        }
+        hare = hare->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    } while(tortoise != NULL && tortoise->next != NULL);
     return 0;
 }
