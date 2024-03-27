ed5686edd09a8a73d4cb028ba624d3dcb95b1440
Arch <topanic@qq.com>
2024-03-28 00:09:59 +0800

    finish su20-lab2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,13 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t bit;
+    /* Get the XOR of bits 0, 2, 3, 5 */
+    bit = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5) ) & 1u;
+    /* Shift register to the right by one bit */
+    *reg = *reg >> 1;
+    /* Insert the XOR bit at the leftmost position */
+    *reg = *reg | (bit << 15);
 }
+
 
--- lab02/lfsr.h
+++ lab02/lfsr.h
@@ -1,1 +1,3 @@
+#include <stdint.h>
+
 void lfsr_calculate(uint16_t *reg);
